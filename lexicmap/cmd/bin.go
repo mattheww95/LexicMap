aa6d4b5febb85db28d1ba7a7077ffa5e3f629106
Matthew Wells <mattwells9@shaw.ca>
2024-09-26 17:08:48 -0500

    created flushing of outputs

--- lexicmap/cmd/bin.go
+++ lexicmap/cmd/bin.go
@@ -9,7 +9,10 @@ import (
 	"os"
 	"path/filepath"
 	"regexp"
+	"runtime"
 	"strings"
+	"time"
+	"unsafe"
 
 	"github.com/shenwei356/bio/seq"
 	"github.com/shenwei356/bio/seqio/fastx"
@@ -20,6 +23,8 @@ import (
 const ShortHeader string = "query\tqlen\thits\tsgenome\tsseqid\tqcovGnm\thsp\tqcovHSP\talenHSP\tpident\tgaps\tqstart\tqend\tsstart\tsend\tsstr\tslen"
 const LongHeader string = "query\tqlen\thits\tsgenome\tsseqid\tqcovGnm\thsp\tqcovHSP\talenHSP\tpident\tgaps\tqstart\tqend\tsstart\tsend\tsstr\tslen\tcigar\tqseq\tsseq\talign"
 const UnspecifiedBin = "NotMapped"
+const UniqueBinned string = "Unique"
+const AllBinned string = "All"
 
 var FastaList []string = []string{".fasta", ".fna", ".fa"}
 var FastqList []string = []string{".fastq", ".fq"}
@@ -29,22 +34,35 @@ var binCmd = &cobra.Command{
 	Short: "Bin input sequences based on the output of search",
 	Long:  "Bin input sequences based on teh output of search",
 	Run: func(cmd *cobra.Command, args []string) {
-		//opt := getOptions(cmd)
+		opt := getOptions(cmd)
 		seq.ValidateSeq = false
 
+		bin_unique := getFlagBool(cmd, "bin-unique-reads")
 		outDirectory := getFlagString(cmd, "out-dir")
 
 		bufferSizeS := getFlagString(cmd, "buffer-size")
 		if bufferSizeS == "" {
 			checkError(fmt.Errorf("value of buffer size. supported unit: K, M, G"))
 		}
+
+		files := getFileListFromArgsAndFile(cmd, args, true, "infile-list", true)
+		if len(files) == 1 {
+			if isStdin(files[0]) {
+				checkError(fmt.Errorf("  no files given, cant read from stdin"))
+			} else {
+				log.Infof("  %d input file given: %s", len(files), files[0])
+			}
+		} else {
+			log.Infof("  %d input file(s) given", len(files))
+		}
+
 		var err error
 		bufferSize, err := ParseByteSize(bufferSizeS)
 		if err != nil {
 			checkError(fmt.Errorf("invalid value of buffer size. supported unit: K, M, G"))
 		}
 
-		//verbose := opt.Verbose
+		verbose := opt.Verbose
 		//outputLog := opt.Verbose || opt.Log2File
 		report := getFlagString(cmd, "report")
 
@@ -60,24 +78,15 @@ var binCmd = &cobra.Command{
 		}
 
 		err = os.Mkdir(outDirectory, 0755)
+		log.Infof("Created output directory: %s", outDirectory)
 		checkError(err)
-
-		files := getFileListFromArgsAndFile(cmd, args, true, "infile-list", true)
-		if len(files) == 1 {
-			if isStdin(files[0]) {
-				log.Info("  no files given, reading from stdin")
-			} else {
-				log.Infof("  %d input file given: %s", len(files), files[0])
-			}
-		} else {
-			log.Infof("  %d input file(s) given", len(files))
-		}
 
 		buf := make([]byte, bufferSize)
 		var fh *xopen.Reader
 		var line string
 		var scanner *bufio.Scanner
 		var blastOutput bool = false
+		log_read_interval := 1000
 
 		//var value string
 		fh, err = xopen.Ropen(report)
@@ -133,25 +142,28 @@ var binCmd = &cobra.Command{
 				allInputs[search_val.query] = append(allInputs[search_val.query], search_val)
 			}
 		}
+		allInputs_l := len(allInputs)
+		log.Infof("Sequences listed in output: %d", allInputs_l)
 
 		checkError(scanner.Err())
 		checkError(fh.Close())
 		var record *fastx.Record
-
-		// maintain a list of output locations in order to obey the restriction on maximum open files at a time
-		outputWrites := make(map[string][]*[]byte)
+		SequenceTracker := 0
+		var BasesInMemory uint64 = 0
+		var FlushBasesThreshold uint64 = 1_000_000_000 // Flush when one million reads are in memory
 
 		log.Info("Assigning queries to genomes.")
 		// Organize output files
 		for _, file := range files {
+			log.Infof("Processing: %s", file)
+
 			// Initialize outputWrites with each genome to be written too
-			for key := range searchGenomes {
-				outputWrites[key] = make([]*[]byte, 0, 1)
-			}
-			outputWrites[UnspecifiedBin] = make([]*[]byte, 0, 1)
+			outputWrites := CreateOutputGroups(&searchGenomes, UnspecifiedBin)
+			outputWrites_unq := CreateOutputGroups(&searchGenomes, UnspecifiedBin)
 			fastxReader, err := fastx.NewReader(nil, file, "")
 
 			checkError(err)
+			timeStart1 := time.Now()
 			for {
 				record, err = fastxReader.Read()
 				if err != nil {
@@ -161,46 +173,108 @@ var binCmd = &cobra.Command{
 					checkError(err)
 					break
 				}
-				fastq_id := string(record.ID)
+				fastq_id := ByteSliceToString(record.ID)
+				BasesInMemory = BasesInMemory + uint64(len(record.Seq.Seq))
+				read := record.Format(0)
 				if val, ok := allInputs[fastq_id]; ok {
-					IdentifyBestHit(val, &outputWrites, record)
+					IdentifyBestHit(val, &outputWrites, &outputWrites_unq, &read, bin_unique)
 					val = nil // remove value from memory
 				} else {
-					read := record.Format(0)
-					outputWrites[UnspecifiedBin] = append(outputWrites[UnspecifiedBin], &read)
-					continue
+					// Unspecified is always unique, but no need to track it twice
+					outputWrites[UnspecifiedBin] = Append(outputWrites[UnspecifiedBin], &read)
+				}
+				SequenceTracker++
+				if (SequenceTracker%log_read_interval) == 0 && verbose {
+					speed := float64(SequenceTracker) / time.Since(timeStart1).Minutes()
+					fmt.Fprintf(os.Stderr, "Processed: %d of %d records %.3f matches per minute\r", SequenceTracker, allInputs_l, speed)
+					if BasesInMemory >= FlushBasesThreshold {
+						// Flush the outputs periodically to prevent the maps from growing too large
+						// causing paging to disk
+						if bin_unique {
+							// TODO outputs not being checked for appending
+							WriteBinnedReads(&outputWrites_unq, file, outDirectory, UniqueBinned, false)
+							WriteBinnedReads(&outputWrites, file, outDirectory, AllBinned, true)
+						} else {
+							WriteBinnedReads(&outputWrites, file, outDirectory, "", true)
+						}
+						BasesInMemory = 0
+					}
 				}
 			}
+
 			fastxReader.Close()
 			log.Infof("Binning records for %s", file)
-			for key, val := range outputWrites {
-				var output_file string
-				if StringContains(file, &FastqList) {
-					output_file = fmt.Sprintf("%s.fastq.gz", key)
-				} else if StringContains(file, &FastaList) {
-					output_file = fmt.Sprintf("%s.fasta.gz", key)
-				} else {
-					checkError(fmt.Errorf("Unrecognized input type %s", file))
-				}
-				output := filepath.Join(outDirectory, output_file)
-				outfh, gw, w, err := outStream(output, true, 1)
-				checkError(err)
-				defer func() {
-					outfh.Flush()
-					if gw != nil {
-						gw.Close()
-					}
-					w.Close()
-				}()
-
-				for _, record := range val {
-					outfh.Write(*record)
-				}
+			if bin_unique {
+				WriteBinnedReads(&outputWrites_unq, file, outDirectory, UniqueBinned, false)
+				WriteBinnedReads(&outputWrites, file, outDirectory, AllBinned, true)
+			} else {
+				WriteBinnedReads(&outputWrites, file, outDirectory, "", true)
 			}
-
+			BasesInMemory = 0
 		}
-
 	},
+}
+
+// / Needed to prevent the overhead of string creation as map keys need to be strings
+// / in golang. This function comes from: https://syslog.ravelin.com/byte-vs-string-in-go-d645b67ca7ff
+func ByteSliceToString(bs []byte) string {
+	return *(*string)(unsafe.Pointer(&bs))
+}
+
+func CreateOutputGroups(genomes *map[string]bool, unspecified_bin string) map[string][]*[]byte {
+	outputWrites := make(map[string][]*[]byte)
+	for key := range *genomes {
+		outputWrites[key] = make([]*[]byte, 0, 1)
+	}
+	outputWrites[unspecified_bin] = make([]*[]byte, 0, 1)
+	return outputWrites
+}
+
+// / Get output file name per a file
+func GetOutputFile(input_file string, output_directory string, output_name string, nested_directory string) string {
+	var output_file string
+	var output string
+	if StringContains(input_file, &FastqList) {
+		output_file = fmt.Sprintf("%s.fastq.gz", output_name)
+	} else if StringContains(input_file, &FastaList) {
+		output_file = fmt.Sprintf("%s.fasta.gz", output_name)
+	} else {
+		checkError(fmt.Errorf("Unrecognized input type %s", input_file))
+	}
+
+	if nested_directory != "" {
+		output = filepath.Join(output_directory, output_file)
+	} else {
+		output = filepath.Join(output_directory, nested_directory, output_file)
+	}
+
+	return output
+}
+
+func WriteBinnedReads(outputs *map[string][]*[]byte, file_name string, output_directory string, nested_string string, clear_records bool) {
+	for key, val := range *outputs {
+		output := GetOutputFile(file_name, output_directory, key, nested_string)
+		outfh, gw, w, err := outStream(output, true, 1)
+
+		checkError(err)
+
+		for _, record := range val {
+			outfh.Write(*record)
+			if clear_records {
+				record = nil
+			}
+		}
+		if clear_records {
+			val = val[:0]
+		}
+		outfh.Flush()
+		if gw != nil {
+			gw.Close()
+		}
+		w.Close()
+	}
+	runtime.GC()
+
 }
 
 // / Check if any string contains a substring
@@ -215,14 +289,19 @@ func StringContains(input string, substr *[]string) bool {
 
 // / A function for future iteration for identification of an
 // / optimal hit per a read if one exists.
-func IdentifyBestHit(search_output []*SearchFields, output_genomes *map[string][]*[]byte, fastx *fastx.Record) {
+func IdentifyBestHit(search_output []*SearchFields, output_genomes *map[string][]*[]byte, output_genomes_unq *map[string][]*[]byte, read *[]byte, id_best bool) {
 	var previous string = ""
+	sgenomes_out := 0
 	for _, value := range search_output {
 		if value.sgenome != previous {
-			read := fastx.Format(0)
-			(*output_genomes)[value.sgenome] = append((*output_genomes)[value.sgenome], &read)
+			sgenomes_out++
+			(*output_genomes)[value.sgenome] = Append((*output_genomes)[value.sgenome], read)
 		}
 		previous = value.sgenome
+	}
+
+	if sgenomes_out == 1 && id_best {
+		(*output_genomes_unq)[previous] = Append((*output_genomes_unq)[previous], read)
 	}
 }
 
@@ -246,6 +325,20 @@ func CheckRegex(line string, header_match string) bool {
 	return false
 }
 
+func Append(slice []*[]byte, new_value ...*[]byte) []*[]byte {
+	n := len(slice)
+	total := len(slice) + len(new_value)
+	if n == cap(slice) {
+		newSize := total * 2 // grow array by 2
+		newSlice := make([]*[]byte, total, newSize)
+		copy(newSlice, slice)
+		slice = newSlice
+	}
+	slice = slice[:total]
+	copy(slice[n:], new_value)
+	return slice
+}
+
 func init() {
 	utilsCmd.AddCommand(binCmd)
 
@@ -257,6 +350,9 @@ func init() {
 
 	binCmd.Flags().StringP("buffer-size", "b", "20M",
 		formatFlagUsage(`Size of buffer, supported unit: K, M, G. You need increase the value when "bufio.Scanner: token too long" error reported`))
+
+	binCmd.Flags().BoolP("bin-unique-reads", "u", true,
+		formatFlagUsage("Create separate reads from unique source into a seperate folder."))
 
 	binCmd.SetUsageTemplate(usageTemplate(""))
 }
